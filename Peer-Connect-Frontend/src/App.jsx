f0f7b6dd330013593e1313ce8498ba897a45c3bf
Samarth Vaidya <v.samarth1212@gmail.com>
2024-09-24 14:12:17 +0530

    Added tailwind eslint support

--- Peer-Connect-Frontend/src/App.jsx
+++ Peer-Connect-Frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from '../Pages/SignUP/SignUp';
 import Login from '../Pages/Login/login';
@@ -7,7 +6,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
