884cd78f50f56d4f2ad8938c8bfae84d23c807fd
I-S-H-I-K-A <ishikaverma174@gmail.com>
2024-09-28 19:52:01 +0530

    hello

--- Peer-Connect-Frontend/src/App.jsx
+++ Peer-Connect-Frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/login/Login';
-import SignUp from './Components/Signup/SignUp';
+//import SignUp from './Components/Signup/SignUp';
+import Signup from './Components/Signup_old/SignUp';
+import Profile from './Profile/Profile';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} ></Route>
       </Routes>
     </Router>
   );
