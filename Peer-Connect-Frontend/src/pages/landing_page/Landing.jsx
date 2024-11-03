cedbce82d17600b3b779e560c747fc86aae6cad3
I-S-H-I-K-A <ishikaverma174@gmail.com>
2024-11-04 02:31:12 +0530

    dashboard page

---
+++ Peer-Connect-Frontend/src/pages/landing_page/Landing.jsx
@@ -0,0 +1,16 @@
+const Landing = () => {
+  return (
+    <div className="h-screen w-screen">
+      <div className="">
+        <div className="">
+          <h1>about</h1>
+        </div>
+        <div className="">
+          <h1>FAQ</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
