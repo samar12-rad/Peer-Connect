f85be9e31e071e3ca918eab69011158a10469167
I-S-H-I-K-A <ishikaverma174@gmail.com>
2024-11-15 18:59:09 +0530

    taking changes

--- Peer-Connect-Frontend/src/pages/chat/FriendRequest.jsx
+++ Peer-Connect-Frontend/src/pages/chat/FriendRequest.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
 import Requestbar from './Requestbar';
 
 const FriendRequest = () => {
+  useEffect(() => {
+    console.log('Friend Request Page');
+    const getData = async () => {
+      const response = await fetch('http://localhost:3000/api/v1/user/data', {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    };
+    getData();
+  }, []);
+
   return (
     <div className="shadow-3d-effect h-[80vh] w-full flex-col overflow-y-scroll rounded-2xl border border-white/5 px-2 py-10 shadow-white backdrop-blur-[7.4px]">
       <h1 className="px-5 pb-7 text-7xl font-bold text-green-500">
--- Peer-Connect-Frontend/src/pages/chat/RequestBar.jsx
+++ Peer-Connect-Frontend/src/pages/chat/RequestBar.jsx
@@ -1,17 +1,20 @@
 const Requestbar = () => {
   return (
-    <div className="mb-5 flex w-full justify-between px-5 py-10">
-      <div className="flex w-fit items-center gap-5">
-        <div className="mb-2 h-10 w-10 rounded-full bg-slate-100"></div>
-        <div>
-          <h1 className="text-3xl">Ishika Verma</h1>
+    <div className="px-15 flex h-fit w-full flex-col items-center justify-center">
+      <div className="h-1 w-[80%] rounded-lg bg-slate-600"></div>
+      <div className="mb-5 flex w-full justify-between px-5 py-10">
+        <div className="flex w-fit justify-center gap-5">
+          <div className="h-10 w-10 rounded-full bg-slate-100"></div>
+          <div>
+            <h1 className="text-3xl">Ishika Verma</h1>
+          </div>
         </div>
-      </div>
 
-      <div className="items-center">
-        <button className="h-12 rounded-lg bg-green-500 p-2 text-lg hover:bg-green-600">
-          Click to view the peer
-        </button>
+        <div className="items-center">
+          <button className="h-12 rounded-lg bg-green-500 p-2 text-lg hover:bg-green-600">
+            Click to view the peer
+          </button>
+        </div>
       </div>
     </div>
   );
